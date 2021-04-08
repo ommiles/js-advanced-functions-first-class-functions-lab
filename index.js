28ff542f8fe0c257684c080b3ea5a15c74a5f086
OM Miles <olivia.m.miles@gmail.com>
2021-04-08 07:45:13 -0700

    Initial assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// should return a new array containing the first two drivers from the passed-in array
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2).map (updatedDrivers => {
+        return updatedDrivers
+    })
+}
+
+// should return an array of the last two drivers
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(Math.max(drivers.length-2, 1)).map (updatedDrivers => {
+        return updatedDrivers
+    })
+}
+
+// allows us to invoke either function from the array
+// has the `returnLastTwoDrivers` function to as its last element
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// should multiply a given value using the created multiplier
+const createFareMultiplier = (multiplier) => {
+    return fare => multiplier * fare
+}
+
+// doubles fares
+const fareDoubler = (fare) => {
+    fare *= 2
+    return fare
+}
+
+// triples fares
+const fareTripler = (fare) => {
+    fare *= 3
+    return fare
+}
+
+// returns the last two drivers when passed returnLastTwoDrivers() as the second argument
+const selectDifferentDrivers = (array, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(array)
+}
+
